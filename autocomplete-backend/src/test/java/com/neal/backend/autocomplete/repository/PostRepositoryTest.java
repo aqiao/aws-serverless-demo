3770bed62686c8281e9f364f30a5004e405fbf8f
He <Jianqiao.He@thomsonreuters.com>
2021-05-31 08:44:23 +0800

    upload some pictures

--- autocomplete-backend/src/test/java/com/neal/backend/autocomplete/repository/PostRepositoryTest.java
+++ autocomplete-backend/src/test/java/com/neal/backend/autocomplete/repository/PostRepositoryTest.java
@@ -4,9 +4,7 @@ import com.neal.backend.autocomplete.model.Post;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
